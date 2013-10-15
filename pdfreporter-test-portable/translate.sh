df16044478b3232b572f04a24776e91899d3e140
Kendra <kendra@inloop.eu>
2013-10-15 09:15:21 +0200

    test translation update

--- pdfreporter-test-portable/translate.sh
+++ pdfreporter-test-portable/translate.sh
@@ -1,9 +1,10 @@
 red='\x1B[1;34m'
 nc='\x1B[0m' # No Color
 
+rm -rf xcode/PDFReporter-test/gen
 echo -e "${red}Translating test-portable...${nc}"
-../pdfreporter-xcode/j2objc/j2objc -classpath ../pdfreporter-test-portable/src:../pdfreporter-test/src -d tmp -use-arc --ignore-missing-imports `find ../pdfreporter-test-portable/src -name '*.java'` | grep error
+../pdfreporter-xcode/j2objc/j2objc -classpath ../pdfreporter-test-portable/src:../pdfreporter-test/src -d xcode/PDFReporter-test/gen -use-arc --ignore-missing-imports `find ../pdfreporter-test-portable/src -name '*.java'` | grep error
 
 echo
 echo -e "${red}Translating test...${nc}"
-../pdfreporter-xcode/j2objc/j2objc  -classpath ../pdfreporter-java/src:../pdfreporter-test-portable/src:../pdfreporter-test/src:../pdfreporter-core/src -d tmp -use-arc --ignore-missing-imports `find ../pdfreporter-test/src -name '*.java'` | grep error
+../pdfreporter-xcode/j2objc/j2objc  -classpath ../pdfreporter-java/src:../pdfreporter-test-portable/src:../pdfreporter-test/src:../pdfreporter-core/src -d xcode/PDFReporter-test/gen -use-arc --ignore-missing-imports `find ../pdfreporter-test/src -name '*.java'` | grep error
