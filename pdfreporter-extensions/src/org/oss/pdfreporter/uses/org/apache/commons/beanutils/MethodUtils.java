167367e47119487ed9fdfc6eaf105abf6689facc
Donat Mueller <dmueller@digireport.ch>
2013-10-20 16:40:31 +0200

    added copyright statements and set under apache 2.0 license

--- pdfreporter-extensions/src/org/oss/pdfreporter/uses/org/apache/commons/beanutils/MethodUtils.java
+++ pdfreporter-extensions/src/org/oss/pdfreporter/uses/org/apache/commons/beanutils/MethodUtils.java
@@ -1,5 +1,3 @@
-package org.oss.pdfreporter.uses.org.apache.commons.beanutils;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,7 +14,7 @@ package org.oss.pdfreporter.uses.org.apache.commons.beanutils;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+package org.oss.pdfreporter.uses.org.apache.commons.beanutils;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
--- pdfreporter-extensions/src/org/oss/pdfreporter/uses/org/w3c/dom/Node.java
+++ pdfreporter-extensions/src/org/oss/pdfreporter/uses/org/w3c/dom/Node.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2004 World Wide Web Consortium,
  *
