152d6171339521787a2e5f2caef2a934d94b22c4
Donat Mueller <dmueller@digireport.ch>
2013-10-20 19:01:08 +0200

    added build script and license information

--- pdfreporter-android/src/org/oss/pdfreporter/xml/parsers/util/XmlParserUnmarshaller.java
+++ pdfreporter-android/src/org/oss/pdfreporter/xml/parsers/util/XmlParserUnmarshaller.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Open Software Solutions GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Open Software Solutions GmbH - initial API and implementation
+ ******************************************************************************/
 package org.oss.pdfreporter.xml.parsers.util;
 
 import javax.xml.parsers.DocumentBuilder;
