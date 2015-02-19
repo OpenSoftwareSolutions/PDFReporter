50b4112bf21f270af31b000d907c26771fd49c52
magnus <mkarlsson@open-software-solutions.ch>
2015-02-19 17:19:22 +0100

    autorelease annotation around testcase
    Former-commit-id: 4a59ece573fdc647cf79fd124af2c4059cc9b16d

--- pdfreporter-test-portable/src/test/org/oss/pdfreporter/PortableTest.java
+++ pdfreporter-test-portable/src/test/org/oss/pdfreporter/PortableTest.java
@@ -6,6 +6,7 @@ import test.org.oss.pdfreporter.ExporterTest;
 import test.org.oss.pdfreporter.RealestateTest;
 import test.org.oss.pdfreporter.providers.JavaTestProvider;
 import test.org.oss.pdfreporter.providers.TestProviderInterface;
+import com.google.j2objc.annotations.AutoreleasePool;
 
 public class PortableTest {
 	
@@ -24,27 +25,30 @@ public class PortableTest {
 			/* Not working in iOS
             Test testAnnotation = method.getAnnotation(Test.class);
             if (testAnnotation != null) {*/
-			if(method.getName().startsWith("export")){
-				try {
-                	System.out.print("Test - "+method.getName());
-                	long time = System.currentTimeMillis();
-                	method.invoke(exporterTest);
-                	System.out.println(" [Done] in "+(System.currentTimeMillis()-time)+"ms");
-                } catch (Exception e) {
-                	System.err.println(" [Failure]");
-                	failure = true;
-                }  
-                try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-				}
-			}
+			failure = runTest(exporterTest, failure, method);
         }
 		if(!failure) {
 			System.out.println("\nExporter tests finished successfully.");
 		} else {
 			System.err.println("\nExporter tests finished with errors.");
 		}
+	}
+
+	@AutoreleasePool
+	private boolean runTest(ExporterTest exporterTest, boolean failure,
+			Method method) {
+		if(method.getName().startsWith("export")){
+			try {
+		    	System.out.print("Test - "+method.getName());
+		    	long time = System.currentTimeMillis();
+		    	method.invoke(exporterTest);
+		    	System.out.println(" [Done] in "+(System.currentTimeMillis()-time)+"ms");
+		    } catch (Exception e) {
+		    	System.err.println(" [Failure]");
+		    	failure = true;
+		    }  
+		}
+		return failure;
 	}
 	
 	public void realestateTest(TestProviderInterface testProvider) {
