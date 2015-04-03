05d307c39de357a10b70824a13fcd985dc57b0e9
magnus <mkarlsson@open-software-solutions.ch>
2015-04-03 18:36:52 +0200

    addRessource implemented
    Former-commit-id: 19e1dbeb02e8f5d3013b6c9a8e42154dfd984636

--- pdfreporter-core/src/org/oss/pdfreporter/compilers/jeval/ChunkBuilder.java
+++ pdfreporter-core/src/org/oss/pdfreporter/compilers/jeval/ChunkBuilder.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Lesser Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl-3.0.html
- * 
+ *
  * Contributors:
  *     Open Software Solutions GmbH - initial API and implementation
  ******************************************************************************/
@@ -18,32 +18,32 @@ import org.oss.pdfreporter.compilers.jeval.IExpressionChunk.ExpresionType;
 
 
 public class ChunkBuilder {
-	
+
 	private final ChunkList chunkList = new ChunkList();
-	
+
 	public ChunkBuilder addText(String text) {
 		chunkList.add(new Chunk(ExpresionType.TYPE_TEXT, text));
 		return this;
 	}
 
-	
+
 	public ChunkBuilder addResource(String text) {
-		chunkList.add(new Chunk(ExpresionType.TYPE_RESOURCE, "str(\"" + text + "\")"));
+		chunkList.add(new Chunk(ExpresionType.TYPE_RESOURCE, text));
 		return this;
 	}
-	
+
 	public ChunkBuilder addVariable(IVariable variable) {
 		chunkList.add(new VariableChunk(variable));
 		return this;
 	}
-	
+
 	public List<IExpressionChunk> getChunkList() {
 		return chunkList;
 	}
-	
+
 	private static class ChunkList extends AbstractList<IExpressionChunk> {
 		private final List<IExpressionChunk> delegate = new ArrayList<IExpressionChunk>();
-		
+
 		@Override
 		public IExpressionChunk get(int index) {
 			return delegate.get(index);
@@ -59,12 +59,12 @@ public class ChunkBuilder {
 			return delegate.add(e);
 		}
 	}
-	
+
 	private static class Chunk implements IExpressionChunk {
-		
+
 		private final ExpresionType type;
 		private final String text;
-		
+
 		Chunk(ExpresionType type, String text) {
 			this.type = type;
 			this.text = text;
@@ -84,10 +84,10 @@ public class ChunkBuilder {
 		public String toString() {
 			return "Chunk [type=" + type + ", text=" + text + "]";
 		}
-		
-		
+
+
 	}
-	
+
 	private static class VariableChunk extends Chunk implements IVariableExpressionChunk {
 
 		private final IVariable variable;
@@ -105,7 +105,7 @@ public class ChunkBuilder {
 		public String toString() {
 			return "VariableChunk [variable=" + variable + "]";
 		}
-		
+
 	}
 
 }
