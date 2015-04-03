5da4f0edcf3be6263c79ebeeb8b81d425603a96e
magnus <mkarlsson@digireport.ch>
2015-04-03 18:35:39 +0200

    new class ExpressionRessource
    Former-commit-id: 12bb239ef45091a9fff3e6f97d2df10f08d12b32

---
+++ pdfreporter-core/src/org/oss/pdfreporter/compilers/jeval/ExpressionRessource.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Open Software Solutions GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Contributors:
+ *     Open Software Solutions GmbH - initial API and implementation
+ ******************************************************************************/
+package org.oss.pdfreporter.compilers.jeval;
+
+import org.oss.pdfreporter.compilers.AbstractExpressionElement;
+import org.oss.pdfreporter.compilers.jeval.IExpressionChunk.ExpresionType;
+import org.oss.pdfreporter.engine.JRValueParameter;
+
+
+public class ExpressionRessource extends AbstractExpressionElement implements IVariable {
+
+	private final IDataHolder data;
+	private final String name;
+
+
+	public ExpressionRessource(IDataHolder data, String name) {
+		this.data = data;
+		this.name = name;
+	}
+
+	@Override
+	public Object getValue() {
+		return getParameter().getValue();
+	}
+
+
+	@Override
+	public Object getVariableHolder() {
+		return getParameter();
+	}
+
+	@Override
+	public ExpresionType getType() {
+		return ExpresionType.TYPE_RESOURCE;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	private JRValueParameter getParameter() {
+		return data.getParameter(getName());
+	}
+
+	@Override
+	public String toString() {
+		return "ExpressionParameter [name=" + name + "]";
+	}
+
+}
