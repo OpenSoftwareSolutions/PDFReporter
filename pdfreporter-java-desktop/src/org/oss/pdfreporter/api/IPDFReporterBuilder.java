1aaa08ca83c4d2a84bc5e9bb5958afad1befee59
magnus <mkarlsson@open-software-solutions.ch>
2015-04-22 18:51:04 +0200

    removed api
    Former-commit-id: afb71f45b7ad668f716e95dcbe881bcbaa7258e4

--- pdfreporter-java-desktop/src/org/oss/pdfreporter/api/IPDFReporterBuilder.java
+++
@@ -1,6 +0,0 @@
-package org.oss.pdfreporter.api;
-
-public interface IPDFReporterBuilder {
-
-
-}
--- pdfreporter-java-desktop/src/org/oss/pdfreporter/api/PDFReporterBuilder.java
+++
@@ -1,5 +0,0 @@
-package org.oss.pdfreporter.api;
-
-public class PDFReporterBuilder implements IPDFReporterBuilder {
-
-}
