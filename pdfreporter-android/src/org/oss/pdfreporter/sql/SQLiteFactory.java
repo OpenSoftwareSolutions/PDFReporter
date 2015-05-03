6fc2a0202cd0293cc415a2847f563caa59d71c34
magnus <mkarlsson@open-software-solutions.ch>
2015-05-03 11:06:17 +0200

    impl. methode
    Former-commit-id: eaf74002311ab55ee6b97fb972b671bc3f116464

--- pdfreporter-android/src/org/oss/pdfreporter/sql/SQLiteFactory.java
+++ pdfreporter-android/src/org/oss/pdfreporter/sql/SQLiteFactory.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Open Software Solutions GmbH - initial API and implementation
  ******************************************************************************/
@@ -16,13 +16,19 @@ import org.oss.pdfreporter.sql.SQLException;
 import org.oss.pdfreporter.sql.factory.AbstractSqlFactory;
 
 public class SQLiteFactory extends AbstractSqlFactory {
-	
+
 	public static void registerFactory() {
 		ApiRegistry.register(new SQLiteFactory());
 	}
-	
+
 	@Override
 	public IConnection newConnection(String url, String user,
+			String password) throws SQLException {
+		return new SQLiteConnection(url);
+	}
+
+	@Override
+	public IConnection createConnection(String url, String user,
 			String password) throws SQLException {
 		return new SQLiteConnection(url);
 	}
