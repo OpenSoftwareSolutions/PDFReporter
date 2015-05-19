3f73f5b38fe1b5aa1926ec5cfae64549dd9b6955
magnus <mkarlsson@digireport.ch>
2015-05-19 08:34:51 +0200

    changed Copyright (c) 2013 to Copyright (c) 2015
    Former-commit-id: cbb0eee7033b82968a2176aeb34c160086ecd27b

--- pdfreporter-portable/src/org/oss/pdfreporter/geometry/Dimension.java
+++ pdfreporter-portable/src/org/oss/pdfreporter/geometry/Dimension.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2013 Open Software Solutions GmbH.
+ * Copyright (c) 2015 Open Software Solutions GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
  * 
  * Contributors:
- *     Open Software Solutions GmbH - initial API and implementation
+ *     Open Software Solutions GmbH
  ******************************************************************************/
 package org.oss.pdfreporter.geometry;
 
