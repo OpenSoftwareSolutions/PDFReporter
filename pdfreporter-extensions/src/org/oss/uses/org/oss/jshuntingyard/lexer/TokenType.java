f6a7b09e55c37f9f27cd0d31ce3aa290dcf3fd90
Magnus Karlsson <mkarlsson@open-software-solutions.ch>
2015-07-13 08:57:04 +0200

    update from jshuntingyard - multiple underscores

--- pdfreporter-extensions/src/org/oss/uses/org/oss/jshuntingyard/lexer/TokenType.java
+++ pdfreporter-extensions/src/org/oss/uses/org/oss/jshuntingyard/lexer/TokenType.java
@@ -3,8 +3,8 @@ package org.oss.uses.org.oss.jshuntingyard.lexer;
 import java.util.regex.Pattern;
 
 public enum TokenType {
-	BOOLEANVALUE("(?i)false|(?i)true"),
-	NULL("(?i)null"),
+	BOOLEANVALUE("false|true", Pattern.CASE_INSENSITIVE),
+	NULL("null", Pattern.CASE_INSENSITIVE),
 	NAN("NaN"),
 	FUNCTIONNAME("[A-Za-z]+([0-9])?"),
 	DIGIT("([-])?([0-9]+(\\.\\d+)?)"),
@@ -13,12 +13,16 @@ public enum TokenType {
 	OPENBRACE("\\("),
 	CLOSEBRACE("\\)"),
 	OPERATOR("\\+|-|\\*|/|==|!=|\\^|\\%|\\|\\||<=|>=|<|>|&&|!"),
-	VARIABLE("\\$([a-zA-Z0-9])+([_])?([a-zA-Z0-9])*");
+	VARIABLE("\\$([a-zA-Z0-9])+(([_])?([a-zA-Z0-9])*)*");
 
 	private final Pattern pattern;
 
 	TokenType(String regularExpression) {
 		this.pattern = Pattern.compile(regularExpression);
+	}
+
+	TokenType(String regularExpression, int pattern) {
+		this.pattern = Pattern.compile(regularExpression, pattern);
 	}
 
 	public Pattern pattern() {
