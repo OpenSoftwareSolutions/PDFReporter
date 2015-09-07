28fb11ae46aab3e17b1071208d9af624f4053506
Magnus Karlsson <mkarlsson@open-software-solutions.ch>
2015-09-07 11:29:30 +0200

    OPT_TEXT_MATCH single & double quoted strings supported

--- pdfreporter-core/src/org/oss/pdfreporter/compilers/jeval/TextConstant.java
+++ pdfreporter-core/src/org/oss/pdfreporter/compilers/jeval/TextConstant.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Lesser Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl-3.0.html
- * 
+ *
  * Contributors:
  *     Open Software Solutions GmbH
  ******************************************************************************/
@@ -16,24 +16,25 @@ import org.oss.pdfreporter.compilers.IExpressionElement;
 import org.oss.pdfreporter.compilers.expressionelements.AbstractExpressionElement;
 
 public class TextConstant extends AbstractExpressionElement implements IExpressionElement{
-	private static String TEXT_MATCH = "^\".*\"$";
-	
+	//private static String TEXT_MATCH = "(^\".*\"$)|(^'.*'$)";
+	private static String OPT_TEXT_MATCH = "('[^'\"]*.*')|(\"[^'\"]*.*\")|([^'\"]*.*)";
+
 	private final String text;
 
 	public static boolean isText(String text) {
-		return text.matches(TEXT_MATCH);
+		return text.matches(OPT_TEXT_MATCH);
 	}
-	
+
 	public static TextConstant parseText(String s) throws ExpressionParseException {
-		if (s.matches(TEXT_MATCH)) {
+		if (s.matches(OPT_TEXT_MATCH)) {
 			return new TextConstant(convertTextChunk(s));
-		} 
+		}
 		throw new ExpressionParseException("Unsupported Text constant " + s);
 	}
-	
-	
+
+
 	/**
-	 * Removes double quotes around text and replaces escaped tabulator, line feed and quotes with tabulator, line feed characters and quote.   
+	 * Removes double quotes around text and replaces escaped tabulator, line feed and quotes with tabulator, line feed characters and quote.
 	 * @param chunkText
 	 * @return
 	 */
@@ -44,8 +45,8 @@ public class TextConstant extends AbstractExpressionElement implements IExpressi
 		chunkText = chunkText.replace("\\\"", "\"");
 		return chunkText;
 	}
-	
-	
+
+
 	public TextConstant(String text) {
 		this.text = text;
 	}
